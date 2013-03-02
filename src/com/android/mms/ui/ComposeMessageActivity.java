12a0137877d77c006219c8e40312dbe3acf31d99
Thibault LEMOINE <thibaultblizz@gmail.com>
2013-03-02 17:01:00 +0100

    Strip Unicode : Fix compilation warnings
    (Patch Set 3)
    
    - Fixed compilation warnings.
    - Other small fixes. (Transformation tabs to spaces)
    
    Signed-off-by: Thibault LEMOINE <thibaultblizz@gmail.com>

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -247,7 +247,7 @@ public class ComposeMessageActivity extends Activity
     private static final int MENU_LOCK_MESSAGE          = 28;
     private static final int MENU_UNLOCK_MESSAGE        = 29;
     private static final int MENU_COPY_TO_DRM_PROVIDER  = 30;
-    private static final int MENU_INSERT_TEMPLATE 		= 31;
+    private static final int MENU_INSERT_TEMPLATE       = 31;
 
     private static final int RECIPIENTS_MAX_LENGTH = 312;
 
