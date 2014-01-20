1a3b5da93e2d103b6e33a8a83b94f42b0120bebb
00raq00 <00raq00@gmail.com>
2014-01-20 21:23:52 +0100

    Possibility to change the size of the MMS
    Change-Id: I3717a803dd4a67b0e890552d66e613bb26fbc0dd

--- src/com/android/mms/ui/MessagingPreferenceActivity.java
+++ src/com/android/mms/ui/MessagingPreferenceActivity.java
@@ -88,6 +88,10 @@ public class MessagingPreferenceActivity extends PreferenceActivity
     public static final String GESTURE_SENSITIVITY      = "pref_key_templates_gestures_sensitivity";
     public static final String GESTURE_SENSITIVITY_VALUE = "pref_key_templates_gestures_sensitivity_value";
 
+    // MMS Size
+    public static final String MMS_SIZE                  = "pref_key_mms_size";
+    public static final String MMS_SIZE_VALUE            = "pref_key_mms_size_value";
+
     // Timestamps
     public static final String FULL_TIMESTAMP            = "pref_key_mms_full_timestamp";
     public static final String SENT_TIMESTAMP            = "pref_key_mms_use_sent_timestamp";
@@ -127,6 +131,7 @@ public class MessagingPreferenceActivity extends PreferenceActivity
     private Preference mSmsDeliveryReportPref;
     private CheckBoxPreference mSmsSplitCounterPref;
     private Preference mMmsLimitPref;
+    private ListPreference mMmsSize;
     private Preference mMmsDeliveryReportPref;
     private Preference mMmsGroupMmsPref;
     private Preference mMmsReadReportPref;
@@ -251,6 +256,9 @@ public class MessagingPreferenceActivity extends PreferenceActivity
         mGestureSensitivity = (ListPreference) findPreference(GESTURE_SENSITIVITY);
         mUnicodeStripping = (ListPreference) findPreference(UNICODE_STRIPPING);
         mUnicodeStrippingEntries = getResources().getTextArray(R.array.pref_unicode_stripping_entries);
+
+        //MMS Size
+        mMmsSize = (ListPreference) findPreference(MMS_SIZE);
 
         // QuickMessage
         mEnableQuickMessagePref = (CheckBoxPreference) findPreference(QUICKMESSAGE_ENABLED);
@@ -379,6 +387,20 @@ public class MessagingPreferenceActivity extends PreferenceActivity
                 int value = Integer.parseInt((String) newValue);
                 sharedPreferences.edit().putInt(GESTURE_SENSITIVITY_VALUE, value).commit();
                 mGestureSensitivity.setSummary(String.valueOf(value));
+                return true;
+            }
+        });
+
+        String mmsSize = String.valueOf(sharedPreferences.getInt(MMS_SIZE_VALUE, (MmsConfig.getMaxMessageSize()/1024)));
+        mMmsSize.setSummary(mmsSize);
+        mMmsSize.setValue(mmsSize);
+        mMmsSize.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                int value = Integer.parseInt((String) newValue);
+                sharedPreferences.edit().putInt(MMS_SIZE_VALUE, value).commit();
+                mMmsSize.setSummary(String.valueOf(value));
+                MmsConfig.setMaxMessageSize(value);
                 return true;
             }
         });
