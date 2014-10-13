6c2d1d18e449360e8cbc877dc6455ca2f4d2430f
Davis Mosenkovs <davikovs@gmail.com>
2014-10-13 14:05:35 -0700

    Fixed crash when repeatedly clicking message during Delivery delay and after send cancellation
    This is reproducible in conversations with many messages (e.g. 7800 messages on SGS3).
    
    Change-Id: I26a7b989c9d27da34481f20f358a9e09bb4a2ef6

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -4441,6 +4441,17 @@ public class ComposeMessageActivity extends Activity
         protected void onQueryComplete(int token, Object cookie, Cursor cursor) {
             switch(token) {
                 case MESSAGE_LIST_QUERY_TOKEN:
+                    /* test validity of cursor */
+                    if (cursor != null && !cursor.isClosed()) {
+                        try {
+                            cursor.moveToLast();
+                        } catch(IllegalStateException e) {
+                            cursor.close();
+                            Log.e(TAG, "Bad cursor.", e);
+                            return;
+                        }
+                    }
+
                     mConversation.blockMarkAsRead(false);
 
                     // check consistency between the query result and 'mConversation'
